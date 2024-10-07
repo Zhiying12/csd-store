84f2f53f5ca4cbfbc8c116696bcd355db09b8ede
Zhiying12 <zhiying.liang@outlook.com>
2024-10-07 15:59:42 -0400

    kernel: a functional kernel, mainly init at the beginning

--- kv-store/src/host.cpp
+++ kv-store/src/host.cpp
@@ -1,4 +1,4 @@
-//#include "cmdlineparser.h"
+#include "cmdlineparser.h"
 //#include "xcl2.hpp"
 #include <atomic>
 #include <fcntl.h>
@@ -18,93 +18,86 @@
 #include "experimental/xrt_device.h"
 #include "experimental/xrt_kernel.h"
 
-// #include "json.h"
-// #include "replicant.h"
-// using nlohmann::json;
+#include "protobuf.h"
 
-int BUFFER_SIZE = 10000;
-int STRUCT_FIELDS = 3;
-int VALUE_NUMS = BUFFER_SIZE * STRUCT_FIELDS;
-int MAX_BUFFER_SIZE = VALUE_NUMS * 4;
-
-struct Entry {
-    int index;
-    int op;
-    int key;
-    int value;
-};
-
-void insert_log_entry(xrt::bo& log_bo, xrt::kernel& krnl, Entry& entry) {
-    auto run = krnl(log_bo, entry, BUFFER_SIZE);
-    if (run) {
-      run.wait();
-    } else
-      std::cout << "false run\n";
-}
+int BUFFER_SIZE = 4096;
 
 void kv_store_apply(xrt::bo& store_bo, xrt::bo& log_bo, xrt::bo& result_bo, xrt::kernel& krnl, int index) {
-    auto run = krnl(store_bo, log_bo, result_bo, index, MAX_BUFFER_SIZE);
+    auto run = krnl(store_bo, log_bo, result_bo, index, BUFFER_SIZE);
     run.wait();
     result_bo.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
 }
 
 int main(int argc, char** argv) {
     // Command Line Parser
-//    sda::utils::CmdLineParser parser;
-//
-//    // Switches
-//    //**************//"<Full Arg>",  "<Short Arg>", "<Description>", "<Default>"
-//    parser.addSwitch("--xclbin_file", "-x", "input binary file string", "kv-hashmap.xcbin");
-//    parser.addSwitch("--device", "-d", "device id", "1");
-//    parser.parse(argc, argv);
+    sda::utils::CmdLineParser parser;
+
+    // Switches
+    //**************//"<Full Arg>",  "<Short Arg>", "<Description>", "<Default>"
+    parser.addSwitch("--xclbin_file", "-x", "input binary file string", "kv-store.xcbin");
+    parser.addSwitch("--device_id", "-d", "device id", "0");
+    parser.addSwitch("--buffer", "-b", "buffer size", "64");
+    parser.addSwitch("--number", "-n", "number of instances", "64");
+    parser.parse(argc, argv);
 
     // Read settings
-    auto binaryFile = "kv-store.xclbin";
-    std::string dev_id = "1";
+    auto binaryFile = parser.value("xclbin_file");
+    std::string dev_id = parser.value("device_id");
+    BUFFER_SIZE = stoi(parser.value("buffer"));
+    auto size = stoi(parser.value("number"));
 
     auto device = xrt::device(dev_id);
     auto uuid = device.load_xclbin(binaryFile);
-    auto krnl = xrt::kernel(device, uuid, "append_instance");
-    auto store_krnl = xrt::kernel(device, uuid, "kv_store_top");
+    auto krnl = xrt::kernel(device, uuid, "kv_store_find");
 
-    auto log_bo = xrt::bo(device, BUFFER_SIZE * sizeof(Entry), krnl.group_id(0));
-    auto store_bo = xrt::bo(device, MAX_BUFFER_SIZE, krnl.group_id(0));
+    auto log_bo = xrt::bo(device, BUFFER_SIZE * sizeof(Instance), krnl.group_id(0));
+    auto result_bo = xrt::bo(device, BUFFER_SIZE * sizeof(Instance), krnl.group_id(1));
+    auto log_bo_map = log_bo.map<Instance *>();
+    auto result_bo_map = result_bo.map<Instance *>();
 
-    // init
-    // for (int i = 0; i < VALUE_NUMS; i++) {
-    // 	kernel_bo_map[i] = 0;
-    // }
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+    	Instance inst;
+    	log_bo_map[i] = inst;
+    	result_bo_map[i] = inst;
+    }
 
-    // std::ifstream f("config.json");
-    // json config;
-    // f >> config;
-    // config["id"] = 0;
-    
-    // boost::asio::io_context io_context(config["threadpool_size"]);
-    // auto replicant = std::make_shared<Replicant>(&io_context, config);
+    for (int i = 0; i < size; i++) {
+        log_bo_map[i].command_.value_ = i;
+    }
+
+    std::cout << result_bo_map[size - 1].command_.value_ << "\n";
+    log_bo.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+    std::cout << "sync to device\n";
+    auto run = krnl(log_bo, result_bo, size);
+    run.wait();
+    std::cout << "run completion\n";
+    result_bo.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+    std::cout << "sync from device\n";
+    std::cout << result_bo_map[size - 1].command_.value_ << "\n";
+
 
     // validation
-    std::atomic<int32_t> index{0};
-    int thread_nums = 20;
-    std::vector<std::thread> threads(thread_nums);
+    // std::atomic<int32_t> index{0};
+    // int thread_nums = 20;
+    // std::vector<std::thread> threads(thread_nums);
 
-    for (int i = 0; i < thread_nums; i++) {
-        threads[i] = std::thread([i, &index, &log_bo, &store_bo, &krnl, &store_krnl, &device] {
-            int op = 0;
-            int next_index = ++index;
-            Entry entry = {next_index, op, i / 2, i / 2 * 100};
-            insert_log_entry(log_bo, krnl, entry);
+    // for (int i = 0; i < thread_nums; i++) {
+    //     threads[i] = std::thread([i, &index, &log_bo, &store_bo, &krnl, &store_krnl, &device] {
+    //         int op = 0;
+    //         int next_index = ++index;
+    //         Entry entry = {next_index, op, i / 2, i / 2 * 100};
+    //         insert_log_entry(log_bo, krnl, entry);
             
-            auto result_bo = xrt::bo(device, 4, krnl.group_id(0));
-            int* result_bo_map = result_bo.map<int *>();
-            kv_store_apply(store_bo, log_bo, result_bo, store_krnl, next_index);
-            std::cout << "Value for thread " << i << " op " << op << ": " << result_bo_map[0] << std::endl;
-        });
-    }
+    //         auto result_bo = xrt::bo(device, 4, krnl.group_id(0));
+    //         int* result_bo_map = result_bo.map<int *>();
+    //         kv_store_apply(store_bo, log_bo, result_bo, store_krnl, next_index);
+    //         std::cout << "Value for thread " << i << " op " << op << ": " << result_bo_map[0] << std::endl;
+    //     });
+    // }
 
-    for (int i = 0; i < thread_nums; i++) {
-    	threads[i].join();
-    }
+    // for (int i = 0; i < thread_nums; i++) {
+    // 	threads[i].join();
+    // }
 
     return 0;
 }
--- kv-store_kernels/src/kv-hashmap.cpp
+++ kv-store_kernels/src/kv-hashmap.cpp
@@ -1,64 +1,110 @@
+#include <stdint.h>
+#include <hls_stream.h>
 #include "protobuf.h"
 
-// Insert a key-value pair into the store
-void append_instance(Instance* log, Instance* cur_instance_bo, Instance* instance) {
-//#pragma HLS PIPELINE
-  int64_t index = cur_instance_bo->index_;
-  // if (instance->ballot_ > log[index].ballot_) {
-    log[index].ballot_ = cur_instance_bo->ballot_;
-    log[index].client_id_ = cur_instance_bo->client_id_;
-    log[index].command_.type_ = cur_instance_bo->command_.type_;
-    log[index].command_.key_ = cur_instance_bo->command_.key_;
-    log[index].command_.value_ = cur_instance_bo->command_.value_;
-    log[index].index_ = cur_instance_bo->index_;
-    log[index].state_ = cur_instance_bo->state_;
-//    *cur_instance_bo = *instance;
-  // }
-}
+#define DATA_SIZE 4096
 
-void kv_put(int* kv_store, int key, int value, int value_nums) {
-//#pragma HLS PIPELINE
-    for (int i = 0; i < value_nums; i += 2) {
-        if (kv_store[i] == 0) {
-            kv_store[i] = key;
-            kv_store[i + 1] = value;
-            return;
-        }
+// TRIPCOUNT identifier
+const int c_size = DATA_SIZE;
+
+// void append_instance(Instance* log, Instance* cur_instance_bo, Instance* instance) {
+// //#pragma HLS PIPELINE
+//   int64_t index = cur_instance_bo->index_;
+//   // if (instance->ballot_ > log[index].ballot_) {
+//     log[index].ballot_ = cur_instance_bo->ballot_;
+//     log[index].client_id_ = cur_instance_bo->client_id_;
+//     log[index].command_.type_ = cur_instance_bo->command_.type_;
+//     log[index].command_.key_ = cur_instance_bo->command_.key_;
+//     log[index].command_.value_ = cur_instance_bo->command_.value_;
+//     log[index].index_ = cur_instance_bo->index_;
+//     log[index].state_ = cur_instance_bo->state_;
+// //    *cur_instance_bo = *instance;
+//   // }
+// }
+
+// void kv_put(int* kv_store, int key, int value, int value_nums) {
+// //#pragma HLS PIPELINE
+//     for (int i = 0; i < value_nums; i += 2) {
+//         if (kv_store[i] == 0) {
+//             kv_store[i] = key;
+//             kv_store[i + 1] = value;
+//             return;
+//         }
+//     }
+// }
+
+// int kv_get(int* kv_store, int key, int value_nums) {
+// //#pragma HLS PIPELINE
+//     for (int i = 0; i < value_nums; i += 2) {
+//         if (kv_store[i] == key) {
+//             return kv_store[i + 1];
+//         }
+//     }
+//     return -1;
+// }
+
+// void kv_store_top(int* kv_store, Command* result_bo, Instance* log, int index, int value_nums) {
+// //#pragma HLS INTERFACE s_axilite port=op
+// //#pragma HLS INTERFACE s_axilite port=key
+// //#pragma HLS INTERFACE s_axilite port=value
+// //#pragma HLS INTERFACE s_axilite port=result
+// //#pragma HLS INTERFACE s_axilite port=return
+
+//     Command cmd = log[index].command_;
+//     result_bo->type_ = log[index].client_id_; 
+//     result_bo->key_ = cmd.key_;
+//     if (cmd.type_ == 0) {
+//         kv_put(kv_store, cmd.key_, cmd.value_, value_nums);
+//         result_bo->value_ = cmd.value_;
+//     } else if (cmd.type_ == 1) {
+//         result_bo->value_ = kv_get(kv_store, cmd.key_, value_nums);
+//     }
+// }
+
+static void read_input(Instance* log, hls::stream<Instance>& inStream, int size) {
+// Auto-pipeline is going to apply pipeline to this loop
+mem_rd:
+    for (int i = 0; i < size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        inStream << log[i];
     }
 }
 
-int kv_get(int* kv_store, int key, int value_nums) {
-//#pragma HLS PIPELINE
-    for (int i = 0; i < value_nums; i += 2) {
-        if (kv_store[i] == key) {
-            return kv_store[i + 1];
-        }
-    }
-    return -1;
-}
-
-void kv_store_top(int* kv_store, Command* result_bo, Instance* log, int index, int value_nums) {
-//#pragma HLS INTERFACE s_axilite port=op
-//#pragma HLS INTERFACE s_axilite port=key
-//#pragma HLS INTERFACE s_axilite port=value
-//#pragma HLS INTERFACE s_axilite port=result
-//#pragma HLS INTERFACE s_axilite port=return
-
-    Command cmd = log[index].command_;
-    result_bo->type_ = log[index].client_id_; 
-    result_bo->key_ = cmd.key_;
-    if (cmd.type_ == 0) {
-        kv_put(kv_store, cmd.key_, cmd.value_, value_nums);
-        result_bo->value_ = cmd.value_;
-    } else if (cmd.type_ == 1) {
-        result_bo->value_ = kv_get(kv_store, cmd.key_, value_nums);
+static void compute_add(hls::stream<Instance>& inStream1,
+                        hls::stream<Instance>& outStream,
+                        int size) {
+// Auto-pipeline is going to apply pipeline to this loop
+execute:
+    for (int i = 0; i < size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        outStream << inStream1.read();
     }
 }
 
-void kv_store_find(Instance* log, Command* result_bo, int size) {
+static void write_result(Instance* result_bo, hls::stream<Instance>& outStream, int size) {
+// Auto-pipeline is going to apply pipeline to this loop
+mem_wr:
+    for (int i = 0; i < size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        result_bo[i] = outStream.read();
+    }
+}
+
+void kv_store_find(Instance* log, Instance* result_bo, int size) {
+    static hls::stream<Instance> inStream1("input_stream_1");
+    static hls::stream<Instance> outStream("output_stream");
+
+#pragma HLS INTERFACE m_axi port = log bundle = gmem0
+#pragma HLS INTERFACE m_axi port = result_bo bundle = gmem0
+
+#pragma HLS dataflow
+	read_input(log, inStream1, size);
+    compute_add(inStream1, outStream, size);
+    write_result(result_bo, outStream, size);
+}
+
+void kv_store_test(Instance* log, Instance* result_bo, int size) {
 	for (int index = 0; index < size; index++) {
-	result_bo->type_ = log[index].client_id_;
-	result_bo->key_ = log[index].command_.key_;
-	result_bo->value_ = log[index].command_.value_;
+	  result_bo[index] = log[index];
 	}
 }
